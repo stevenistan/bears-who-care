285aa60168977373315736578b03c27b8fd0eab6
Steven Tan <tan.steven.97@gmail.com>
2017-10-28 05:57:05 -0700

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-p1 = Post.create(content: "Testing")
+p1 = Post.create(content: "In CS61A, I found myself immersed in a world I knew nothing about. Concepts which took others mere minutes, took me days to understand. “We learned this in AP Computer Science,” my classmates would brag. Unfortunately, I did not have that privilege. Connecting ideas for the first time was like putting together a puzzle, except blindfolded. With all of my free time I spent in queues for office hours and attending guerrilla sessions, I constantly felt like I was falling behind. Studying every day just wasn’t good enough to catch up to my peers who had more experience, or rather, I wasn’t good enough. To exacerbate my already demoralized condition, I constantly disparaged myself. “Go major in something else, you computer science dropout.” Maybe I just wasn’t cut out for computer science. Or maybe, I just wasn’t cut out for Berkeley. With such overwhelming disappointment in myself, I spiraled into a black hole of depression.")
